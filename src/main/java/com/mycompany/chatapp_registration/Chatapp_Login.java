78272c7b0fdaff43018d3c3b21c361aa4f24b4c5
koket <koket@ThenameKaz>
2026-06-13 14:27:05 +0200

    added suitable comment

--- src/main/java/com/mycompany/chatapp_registration/Chatapp_Login.java
+++ src/main/java/com/mycompany/chatapp_registration/Chatapp_Login.java
@@ -16,7 +16,7 @@ public class Chatapp_Login {
     String firstName;
     String lastName;
     
-    //Method
+    //Method checks if username is valid
   boolean checkUserName(String username){
       
       if(username.length() <=5 && username.contains("_")){
@@ -25,16 +25,19 @@ public class Chatapp_Login {
           return false;
     }      
   }
-    //Method
+    //Method checks if password is strong enough
   boolean checkPasswordComplexity(String password){
+      
+      //this will check the length
      if(password.length() <8){
          return false;
     }
-  
+  // these flags start as false and get set to true when requirements are reached
   boolean hasCapital = false;
   boolean hasNumber = false;
   boolean hasSpecialCharacters = false;
  
+  //goes through every character and check what it is
   for (int i = 0; i < password.length(); i++) {
       char c = password.charAt(i);
   
@@ -44,10 +47,12 @@ public class Chatapp_Login {
     if (Character.isDigit(c)){
         hasNumber = true;
 }
+    //a special character is anything that is not a letter or number
   if(!Character.isLetterOrDigit(c)){
     hasSpecialCharacters = true;
     } 
 }
+  // all three must be true for the paasword to pass
   if (hasCapital && hasNumber && hasSpecialCharacters){
   return true;
 }else{
@@ -60,6 +65,7 @@ public class Chatapp_Login {
     //Available at: <https://stackoverflow.com/questions/tagged/regex+phone-number>
     //[Accessed 9 April 2026]
   
+  //method checks if cell phone number is valid
   boolean checkCellPhoneNumber(String cellNumber){
       if (cellNumber.matches("^\\+[0-9]{11}$")){
           return true;
@@ -67,37 +73,40 @@ public class Chatapp_Login {
           return false;
       }
     }
-    
+    //method rruns all three checks and returms result messages
     String registerUser(String username, String password, String cellNumber){
-        //Variables
+        //Variables that hwill hold the message for each check
         String usernameMessage;
         String passwordMessage;
         String cellNumberMessage;
         
+        // checks the username and save it if it is valid
         if(checkUserName(username)){
             usernameMessage = "Username successfully captured.";
             this.username = username;
         }else{
             usernameMessage = "Username is not correctly formatted; please ensure that your username contains an underscore and is no more than five characters in length.";
         }
+        //check the password and saves it if its valid
         if(checkPasswordComplexity(password)){
             passwordMessage = "Password successfully captured.";
             this.password = password;
         }else{
             passwordMessage = "Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number, and a special character";
         }
-        
+        //checks cell phone number and saves it if it is valid
         if (checkCellPhoneNumber(cellNumber)){
             cellNumberMessage = "Cell number successfully captured.";
             this.cellNumber = cellNumber;
         }else{
             cellNumberMessage="Cell number incorrectly formatted or does not contain international code; please correct the number and try again.";
         }
-        
+        // returns all three messages together on separate lines
         return usernameMessage + "\n" + passwordMessage + "\n" + cellNumberMessage;
       
  }
-    //Method
+    //Method checks if the login details match what was registered
+    //returns true if the match and false if they don't
     boolean loginUser(String username, String password){
         if ((this.username.equals(username) && this.password.equals(password))){
             return true;
@@ -105,7 +114,7 @@ public class Chatapp_Login {
             return false;
         }
     }
-    //Method
+    //Method returns the right message depending if the login eas successfull or not
     public String returnLoginStatus(String username, String password){
         if (loginUser(username, password)){
             return "Welcome " + firstName + ", " + lastName + " it is great to see you.";
